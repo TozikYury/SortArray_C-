31d4a88438832e370049575d178aef071f12a86f
Tozik Yury <urijtozik@gmail.com>
2024-10-30 20:22:41 +0300

    Проверка на правильность ввода

--- app.cpp
+++ app.cpp
@@ -1,36 +1,22 @@
 #include <iostream>
-
+#include <limits>
 using namespace std;
 
-int summ(a,b);
-
-int main(int argc, char const *argv[])
+int main()
 {
-    int numberOne, numberTwo;
-    int summ;
-    try
+
+    int n, a;
+
+    while (!(cin >> n))
     {
-        
+
+        cin.clear();
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        cout << "Некоректный ввод даных! Введите их ещё раз: ";
     }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+    a = n;
     
-    cout << "Enter number One" << endl;
-    cin >> numberOne;
-    cout << "Enter number Two" << endl;
-    cin >> numberTwo;
-    if (typeid(numberOne) == typeid(int) && typeid(numberTwo) == typeid(int))
-    {
-        summ = numberOne + numberTwo;
-    }
-    else
-       //throw std::string{"-----------"};
-       return 0;
-
-    cout << "Summ numbers:=" << summ << endl;
-
-    return 0;  
+    cout << a << endl;
 }
-
