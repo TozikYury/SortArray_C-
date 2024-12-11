50434c03ebcc00af1d9e300b0a734715aba8dfcb
Tozik Yury <urijtozik@gmail.com>
2024-12-11 20:37:57 +0300

    Дороботаный вывод массива

--- app.cpp
+++ app.cpp
@@ -2,23 +2,30 @@
 #include <iostream>
 #include <unistd.h>
 
-std::string gen_random(const int len) {
+using namespace std;
+
+string random_array(const int len)
+{
     static const char alphanum[] =
         "0123456789"
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         "abcdefghijklmnopqrstuvwxyz";
-    std::string tmp_s;
+    string tmp_s;
     tmp_s.reserve(len);
 
-    for (int i = 0; i < len; ++i) {
+    for (int i = 0; i < len; ++i)
+    {
         tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
     }
 
     return tmp_s;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    static const int sizeArray = 100;
     srand((unsigned)time(NULL) * getpid());
-    std::cout << gen_random(100) << "\n";
+
+    cout << random_array(sizeArray) << "\n";
     return 0;
 }
