619c721aa996c8308184b428610d59cc385bc00a
Tozik Yury <urijtozik@gmail.com>
2024-12-25 10:26:50 +0300

    Готовая сортировка

--- app.cpp
+++ app.cpp
@@ -1,35 +1,47 @@
-#include <ctime>
-#include <iostream>
-#include <unistd.h>
+#include<iostream>
+#include <math.h>
 
-using namespace std;
 
-static const int sizeArray = 100;
 
-string random_array(const int len)
-{
-    static const char alphanum[] =
-        "0123456789"
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "abcdefghijklmnopqrstuvwxyz";
-    string tmp_s;
-    tmp_s.reserve(len);
+const int SIZE = 100;
 
-    for (int i = 0; i < len; ++i)
-    {
-        tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
-    }
-
-    return tmp_s;
+void fillArray(char* arr, char choice) {
+if (choice == '1') {
+std::cout << "Enter " << SIZE << " characters (only letters and digits): ";
+for (int i = 0; i < SIZE; ++i) {
+std::cin >> arr[i];
+}
+} else if (choice == '2') {
+char A, B;
+std::cout << "Enter range A and B: ";
+std::cin >> A >> B;
+for (int i = 0; i < SIZE; ++i) {
+arr[i] = rand() % (B - A + 1) + A;
+}
+}
 }
 
-int main(int argc, char *argv[])
-{
-    char number(126);
+void bubbleSort(char* arr, int size) {
+for (int i = 0; i < size - 1; ++i) {
+for (int j = 0; j < size - i - 1; ++j) {
+if (arr[j] > arr[j + 1]) {
+char temp = arr[j];
+arr[j] = arr[j + 1];
+arr[j + 1] = temp;
+}
+}
+}
+}
 
-    srand((unsigned)time(NULL) * getpid());
-    cout << "Вывод исходного масива" << endl;
-    cout << random_array(sizeArray) << "\n";
-    cout << "Char ="<< number << endl;
-    return 0;
+void countCharacters(char* arr) {
+int count[62] = {0}; // 26 letters + 10 digits = 36; 26 capital letters + 10 digits = 62
+for (int i = 0; i < SIZE; ++i) {
+if (arr[i] >= '0' && arr[i] <= '9') {
+count[arr[i] - '0']++;
+} else if (arr[i] >= 'A' && arr[i] <= 'Z') {
+count[arr[i] - 'A' + 10]++;
+} else if (arr[i] >= 'a' && arr[i] <= 'z') {
+count[arr[i] - 'a' + 36]++;
+}
+}
 }
